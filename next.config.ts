603667427f73732f47f41abba75791d952b6b6e5
İrşad Türker <wspawned@gmail.com>
2024-11-11 03:49:13 +0300

    ProductPhotos component and photo selection functionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'n11scdn.akamaized.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,16 @@ import ProductInfo from './components/ProductInfo';
 import { useState } from 'react';
 import { ProductVariant } from '@/types/models';
 import { Suspense } from 'react'
+import ProductPhotos from './components/ProductPhotos';
 
 export default function Home() {
   const [selectedVariant, setSelectedVariant] = useState<ProductVariant | null>(null);
 
   return (
     <div className="flex flex-row w-full h-full items-center justify-items-center min-h-screen p-2 md:p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-row w-full h-full">        
-        <div className='flex flex-col w-2/5 bg-red-400 ' >
-          PRODUCT PHOTOS
-        </div>
+      <main className="flex flex-row w-full h-full">
         <Suspense>
+          <ProductPhotos data = {productData} selectedVariant = {selectedVariant}/>
           <ProductInfo data = {productData} selectedVariant = {selectedVariant} setSelectedVariant = {setSelectedVariant}/>
         </Suspense>
       </main>
