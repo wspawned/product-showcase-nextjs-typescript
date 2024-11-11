0065281c365021c7be02193990366329cfdf442e
İrşad Türker <wspawned@gmail.com>
2024-11-11 05:06:48 +0300

    ProductPhotos Css

--- src/app/components/ProductPhotos.tsx
+++ src/app/components/ProductPhotos.tsx
@@ -26,29 +26,29 @@ const ProductPhotos:React.FC<{data: ProductData, selectedVariant: ProductVariant
   }
 
   return (
-    <div className='flex flex-col w-2/5 items-center p-4' >
-      <div className="w-[600px] h-[600px]">
+    <div className='flex flex-col w-full md:w-2/5 items-center md:p-4' >
+      <div className="w-[300px] h-[300px] md:w-[600px] md:h-[600px] mt-6 md:mt-0">
         {displayPhoto && <Image 
           src={displayPhoto} 
           alt="Displayed Photo" 
-          width={800} 
-          height={400} 
+          width={600} 
+          height={600} 
           className="w-full h-full object-contain"
           priority={true}
         />}
       </div>
 
-      <div className="grid grid-cols-4 w-full gap-4 mt-8">
+      <div className="grid grid-cols-4 w-full gap-2 md:gap-4 mt-8 mx-auto">
         {photoList && photoList.map((photo: string, index: number) => (
-          <div className={`w-[160px] h-[160px] border-2 border-gray-300 hover:cursor-pointer ${photo === displayPhoto && "border-4 border-slate-600"}`}
+          <div className={`w-[80px] h-[80px] md:w-[160px] md:h-[160px] border-2 border-gray-300 hover:cursor-pointer ${photo === displayPhoto && "md:border-4 border-gray-600"}`}
           key={index}
           onClick={() => showInDisplay(photo)}
           >
             <Image 
             src={photo} 
             alt={`Thumbnail ${index + 1}`}
-            width={800} 
-            height={400} 
+            width={160} 
+            height={160} 
             className="w-full h-full object-contain" 
             />
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   const [selectedVariant, setSelectedVariant] = useState<ProductVariant | null>(null);
 
   return (
-    <div className="flex flex-row w-full h-full items-center justify-items-center min-h-screen p-2 md:p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-row w-full h-full">
+    <div className="flex flex-row w-full h-full items-start md:items-center justify-items-center min-h-screen p-2 md:p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col md:flex-row w-full h-full">
         <Suspense>
           <ProductPhotos data = {productData} selectedVariant = {selectedVariant}/>
           <ProductInfo data = {productData} selectedVariant = {selectedVariant} setSelectedVariant = {setSelectedVariant}/>
